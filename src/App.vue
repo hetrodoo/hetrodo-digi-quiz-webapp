56f932f08400defa5f5179428e27057e92d6bc8f
hetrodoo <eduleite62@gmail.com>
2023-02-15 12:26:46 -0300

    Removed default vue pages.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <qs-container absolute>
+    <qs-button
+      background-color="var(--secondary)"
+      color="#FFF"
+    >
+      Avançar
+    </qs-button>
+
+    <qs-progress :progress="25" />
+
+    <router-view/>
+  </qs-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap')
+*
+  --primary: #f5f1e5
+  --secondary: #e52729
+  font-family: 'Roboto', sans-serif
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+body, html
+  background-color: var(--primary)
+  margin: 0
+  padding: 0
+  min-width: 100%
+  min-height: 100%
 </style>
+<script setup lang="ts">
+import QsButton from '@/components/primitives/qsButton.vue'
+import QsContainer from '@/components/primitives/qsContainer.vue'
+import QsProgress from '@/components/primitives/qsProgress.vue'
+</script>
