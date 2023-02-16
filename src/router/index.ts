a51fb614de30f6a1cb4ca0145588fff06485d765
hetrodoo <eduleite62@gmail.com>
2023-02-16 00:14:37 -0300

    Add result view.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import QuizView from '../views/QuizView.vue'
+import ResultView from '../views/ResultView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'quiz',
     component: QuizView
+  },
+  {
+    path: '/result/:percentage',
+    name: 'result',
+    component: ResultView
   }
 ]
 
---
+++ src/views/ResultView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="center">
+    Seu Resultado foi: {{ route.params.percentage }}%
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<style scoped lang="sass">
+.center
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate(-50%, -50%)
+  font-size: 24px
+</style>
